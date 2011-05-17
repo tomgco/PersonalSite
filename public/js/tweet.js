995fda83553b6d18e9cd813a29975135944d495d
tomgallacher <tomgallacher23@gmail.com>
2011-05-17 02:42:43 +0100

    Added matches to Urls so that you can click

--- public/js/tweet.js
+++ public/js/tweet.js
@@ -28,8 +28,10 @@
 						element += '<div class="tweet">' + createTweetContainerAndAppend(data[i]).text() + '</div>';
 					}
 				}
-				var tweets = element.replace(/(^|[^a-z0-9_])@([a-z0-9_]+)/gi, "$1@<a href=\"https://www.twitter.com/$2\">$2</a>");
+				var tweets = element.replace(/([A-Za-z]{3,9}):\/\/([-;:&=\+\$,\w]+@{1})?([-A-Za-z0-9\.]+)+:?(\d+)?((\/[-\+~%/\.\w]+)?\??([-\+=&;%@\.\w]+)?#?([\w]+)?)/g,
+	"<a target=\"_blank\" href=\"$&\">$&</a>");
 				tweets = tweets.replace(/(^|[^a-z0-9_])#([a-z0-9_]+)/gi, "$1#<a href=\"https://twitter.com/#!/search/%23$2\">$2</a>");
+				tweets = tweets.replace(/(^|[^a-z0-9_])@([a-z0-9_]+)/gi, "$1@<a href=\"https://www.twitter.com/$2\">$2</a>");
 				element = tweets;
 				$(that).html(element);
 				
