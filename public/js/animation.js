40560af9635cb4bd270a2906c3e1b5faf72c0cd5
Tom Gallacher <tomgallacher23@gmail.com>
2011-08-09 15:14:38 +0100

    Removing animation on pages other than the terminal

--- public/js/animation.js
+++ public/js/animation.js
@@ -6,23 +6,24 @@ $(function() {
 	var height = elmnt.height() / 2;
 	var offset = elmnt.offset();
 
-
-	elmnt.mousemove(function(e){
-		var x = e.pageX - offset.left;
-		var y = e.pageY - offset.top;
+	if ($('#main-content').children('#terminal').length > 0) {
+		elmnt.mousemove(function(e){
+			var x = e.pageX - offset.left;
+			var y = e.pageY - offset.top;
 		
-		if ((x < width) && (y < height)) {
-			elmnt.attr('class', 'top-left');
-		} else if ((x < width) && (y > height)) {
-			elmnt.attr('class', 'bottom-left');
-		} else if ((x > width) && (y < height)) {
-			elmnt.attr('class', 'top-right');
-		} else if ((x > width) && (y > height)) {
-			elmnt.attr('class', 'bottom-right');
-		}
-	});
+			if ((x < width) && (y < height)) {
+				elmnt.attr('class', 'top-left');
+			} else if ((x < width) && (y > height)) {
+				elmnt.attr('class', 'bottom-left');
+			} else if ((x > width) && (y < height)) {
+				elmnt.attr('class', 'top-right');
+			} else if ((x > width) && (y > height)) {
+				elmnt.attr('class', 'bottom-right');
+			}
+		});
 	
-	elmnt.mouseout(function(e) {
-		elmnt.attr('class', 'normal');
-	});
+		elmnt.mouseout(function(e) {
+			elmnt.attr('class', 'normal');
+		});
+	}
 });
